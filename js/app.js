9239f689944c59b2cd15b525066918f828d2cd5a
s3su <sebastiansuarez@sesu.local>
2015-04-24 11:50:25 -0300

    SS v0.02

--- js/app.js
+++ js/app.js
@@ -143,9 +143,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
       }
     }
   })
-
-
-    
     
   .state('app.search', {
     url: "/search",
@@ -184,8 +181,18 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
         templateUrl: "templates/profileVsPyramid.html"
       }
     }
-  });
+  })
 
+  .state('app.profile', {
+	    url: "/profile",
+	    views: {
+	      'menuContent': {
+	        templateUrl: "templates/profile.html"
+	      }
+	    }
+	  });
+
+  
     // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/browse');
+  $urlRouterProvider.otherwise('/app/profile');
 });
